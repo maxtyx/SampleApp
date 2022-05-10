9a212aa3161bb39e64782ea018758b347c8aed9f
Maximilian Tan <91178687+maxtyx@users.noreply.github.com>
2022-05-10 11:43:44 +0800

    Add files via upload

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Max on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
